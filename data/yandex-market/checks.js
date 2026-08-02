21523cab17f39d3bac7180dbf5fe51c20faeeddb
otekina98-source <otekina98@gmail.com>
2026-08-02 21:24:42 +0300

    Add files via upload

---
+++ data/yandex-market/checks.js
@@ -0,0 +1,17 @@
+const ymChecks = [
+    {
+        id: 'ym-sverka-1',
+        title: 'Сверка ШК и реестр',
+        path: 'data/yandex-market/shk-reestr/index.html'
+    },
+    {
+        id: 'ym-sverka-2',
+        title: 'Сверка АКТ и реестр',
+        path: 'data/yandex-market/akt-reestr/index.html'
+    },
+    {
+        id: 'ym-sverka-3',
+        title: 'Сверка по грузоместам',
+        path: 'data/yandex-market/gruzomesta/index.html'
+    }
+];
