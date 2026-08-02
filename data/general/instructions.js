69c97cdbdcf8ff21164615e61bfd75c15a8725ba
otekina98-source <otekina98@gmail.com>
2026-08-02 21:21:41 +0300

    Delete data directory

--- data/general/instructions.js
+++
@@ -1,9 +0,0 @@
-const allInstructions = [
-    {
-        id: 'site-instruction',
-        title: 'Инструкция по работе с сайтом',
-        isMainInstruction: true,
-        steps: [],
-        messageTemplate: null
-    }
-];
